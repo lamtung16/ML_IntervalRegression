7a05f1fb0047d3ea13870c396f39a26810238632
Tung Nguyen <nguyenlamtung10@gmail.com>
2025-01-27 10:02:32 -0700

    temp

---
+++ model/mmit/run_one.py
@@ -0,0 +1,66 @@
+import pandas as pd
+import torch
+import torch.nn as nn
+import numpy as np
+import random
+import os
+import sys
+from sklearn.model_selection import KFold
+from mmit_functions import mmit
+
+# Set random seed for reproducibility
+random_seed = 42
+torch.manual_seed(random_seed)
+torch.cuda.manual_seed_all(random_seed)
+np.random.seed(random_seed)
+random.seed(random_seed)
+
+# Load parameters
+params_df = pd.read_csv("params.csv")
+param_row = int(sys.argv[1])
+params = params_df.iloc[param_row]
+
+dataset    = params['dataset']
+test_fold  = params['test_fold']
+
+# create folder for predictions
+os.makedirs(f'predictions', exist_ok=True)
+
+# load data
+folds_df = pd.read_csv(f'../../data/{dataset}/folds.csv')
+features_df = pd.read_csv(f'../../data/{dataset}/features.csv').astype(np.float32)
+target_df = pd.read_csv(f'../../data/{dataset}/targets.csv').astype(np.float32)
+train_indices = folds_df[folds_df['fold'] != test_fold].index
+test_indices = folds_df[folds_df['fold'] == test_fold].index
+
+X_train = features_df.loc[train_indices].values
+X_test = features_df.loc[test_indices].values
+y_train = target_df.loc[train_indices].values
+
+max_depths = [0, 5, 10, 15, 20, 25, 30]
+kf = KFold(n_splits=5, shuffle=True, random_state=42)
+best_models = []
+for train_idx, val_idx in kf.split(X_train):
+    X_subtrain, X_val = X_train[train_idx], X_train[val_idx]
+    y_subtrain, y_val = y_train[train_idx], y_train[val_idx]
+    best_model = None
+    best_hinge_error = float('inf')
+    for max_depth in max_depths:
+        tree = mmit(max_depth=max_depth)
+        tree.fit(X_subtrain, y_subtrain)
+
+        # Predict on validation set
+        y_val_pred = tree.predict(X_val)
+
+        # Compute hinge error
+        y_val_low = y_val[:, 0] + tree.margin_length
+        y_val_up = y_val[:, 1] - tree.margin_length
+        hinge_error = np.sum(tree.hinge_error(y_val_pred, y_val_low, y_val_up))
+
+        if hinge_error < best_hinge_error:
+            best_hinge_error = hinge_error
+            best_model = tree
+    best_models.append(best_model)
+target_mat_pred = np.mean([model.predict(X_test) for model in best_models], axis=0)
+prediction = pd.DataFrame({'pred': target_mat_pred})
+prediction.to_csv(f"predictions/{dataset}.{test_fold}.csv", index=False)
