bc3d1746585b53d940ff683d1c011dc411c7306c
Tung Nguyen <nguyenlamtung10@gmail.com>
2025-02-12 08:53:26 -0700

    add aft_xgboost normal

--- model/aft_xgboost/run_one.py
+++ model/aft_xgboost/run_one.py
@@ -5,6 +5,8 @@ import sys
 import xgboost as xgb
 import os
 from sklearn.model_selection import ParameterGrid
+from sklearn.model_selection import KFold
+import copy
 
 # Create folder for predictions
 os.makedirs('predictions', exist_ok=True)
@@ -36,65 +38,52 @@ X_train = features_df.loc[train_ids].values
 y_train = target_df.loc[train_ids].values
 X_test = features_df.loc[test_ids].values
 
+# Log-transform target values
+y_train = np.exp(y_train)
+
 # Define parameter grid
 param_grid = {
     'objective': ['survival:aft'],
     'eval_metric': ['aft-nloglik'],
+    'tree_method': ['hist'],
     'aft_loss_distribution': ['normal'],
     'aft_loss_distribution_scale': [0.5, 1.0, 1.5],
-    'tree_method': ['hist'],
     'learning_rate': [0.001, 0.1, 1.0],
-    'max_depth': [2, 4, 6, 8, 10],
+    'max_depth': [2, 4, 6, 8, 10, 20],
     'min_child_weight': [0.001, 1.0, 10.0],
     'reg_alpha': [0.001, 1.0, 10.0],
     'reg_lambda': [0.001, 1.0, 10.0]
 }
 
-def perform_cross_validation(params, dtrain):
-    cv_results = xgb.cv(
-        params,
-        dtrain,
-        num_boost_round=2000,
-        nfold=5,
-        early_stopping_rounds=50,
-        as_pandas=True,
-        seed=42  # Set random seed for reproducibility
-    )
-    return cv_results
+# Perform K-Fold Cross Validation
+kf = KFold(n_splits=5, shuffle=True, random_state=random_seed)
 
-y_lower_bound, y_upper_bound = y_train[:, 0], y_train[:, 1]
+best_models = []
+for train_idx, val_idx in kf.split(X_train):
 
-# Create DMatrix with bounds
-dtrain = xgb.DMatrix(X_train)
-dtrain.set_float_info('label_lower_bound', y_lower_bound)
-dtrain.set_float_info('label_upper_bound', y_upper_bound)
+    dtrain = xgb.DMatrix(X_train[train_idx])
+    dtrain.set_float_info('label_lower_bound', y_train[train_idx][:, 0])
+    dtrain.set_float_info('label_upper_bound', y_train[train_idx][:, 1])
 
-# Perform cross-validation to find the best parameters
-best_params = None
-best_cv_score = float('inf')
-for params in ParameterGrid(param_grid):
-    cv_results = perform_cross_validation(params, dtrain)
-    mean_score = cv_results['test-aft-nloglik-mean'].mean()  # Minimize the negative log-likelihood
+    dval = xgb.DMatrix(X_train[val_idx])
+    dval.set_float_info('label_lower_bound', y_train[val_idx][:, 0])
+    dval.set_float_info('label_upper_bound', y_train[val_idx][:, 1])
 
-    if mean_score < best_cv_score:
-        best_cv_score = mean_score
-        best_params = params
+    best_model = None
+    best_val_loss = float('inf')
+    for params in ParameterGrid(param_grid):
+        model = xgb.train(params, dtrain, num_boost_round=5000, evals=[(dval, 'val')], early_stopping_rounds=50, verbose_eval=False)
+        val_loss = model.best_score
+        if val_loss < best_val_loss:
+            best_model = copy.deepcopy(model)
+            best_val_loss = val_loss
+    best_models.append(best_model)
 
-# Use the best parameters from cross-validation
-params = best_params
-
-bst = xgb.train(
-    params, 
-    dtrain, 
-    num_boost_round=2000, 
-    evals=[(dtrain, 'train')],
-    early_stopping_rounds=50
-)
-
-# Create DMatrix for prediction
+# Make predictions on the test set
 dtest = xgb.DMatrix(X_test)
-
-# Predict
-y_pred = bst.predict(dtest)
+y_pred = np.zeros(X_test.shape[0])
+for model in best_models:
+    y_pred += np.log(model.predict(dtest))
+y_pred /= len(best_models)
 prediction_df = pd.DataFrame({'pred': y_pred})
 prediction_df.to_csv(f"predictions/{dataset}.{test_fold}.csv", index=False)
